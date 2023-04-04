e1699a7f48082a89c7563917f0fea68a48135248
Студент <Student@College.edu>
2023-04-04 20:29:44 +0300

    peredelka

---
+++ Столовые приборы/StoloviePribori/StoloviePribori/ProductForList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace StoloviePribori
+{
+    public class ProductForList
+    {
+        public Brush Background { get; set; }
+        public ImageSource ProductPhoto { get; set; }
+        public int ProductId { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductManufacturer { get; set; }
+        public string ProductCost { get; set; }
+        public string ProductQuantityInStock { get; set; }
+    }
+}
--- Столовые приборы/StoloviePribori/StoloviePribori/StoloviePriboriWindow.xaml.cs
+++ Столовые приборы/StoloviePribori/StoloviePribori/StoloviePriboriWindow.xaml.cs
@@ -19,19 +19,27 @@ namespace StoloviePribori
     /// </summary>
     public partial class StoloviePriboriWindow : Window
     {
+        private Base.Product selectedItem;
         private Base.User user;
+        private bool justChange = true;
         public StoloviePriboriWindow(Base.User user)
         {
             InitializeComponent();
             this.user = user;
             if(user != null)
             {
+                if(user.UserRole == 1)
+                {
+                    EditProduct.Visibility = Visibility.Visible;
+                    AddProduct.Visibility = Visibility.Visible;
+                }
                 userNameLabel.Content = user.UserSurname + " " + user.UserName + " " + user.UserPatronymic;
             }
             else
             {
                 userNameLabel.Content = "Вы вошли как Гость!";
             }
+            loadProducts();
         }
 
         private void BackToAuth_Click(object sender, RoutedEventArgs e)
@@ -40,9 +48,101 @@ namespace StoloviePribori
             Close();
         }
 
-        private void openProductsPage_Click(object sender, RoutedEventArgs e)
+        private void loadProducts()
         {
-            frameForPages.Navigate(new Pages.ProductsPage(user));
+            List<Base.Product> products = SourceCore.entities1.Product.ToList();
+            List<ProductForList> productForLists = new List<ProductForList>();
+            for (int i = 0; i < products.Count; i++)
+            {
+                ImageSource source = new BitmapImage(new Uri("/Images/picture.png", UriKind.RelativeOrAbsolute));
+                productForLists.Add(new ProductForList()
+                {
+                    Background = products[i].ProductQuantityInStock == 0 ? (Brush)FindResource("Color9") : (Brush)FindResource("Color2"),
+                    ProductPhoto = source,
+                    ProductId = products[i].ProductArticleNumber,
+                    ProductDescription = products[i].ProductDescription,
+                    ProductManufacturer = products[i].ProductManufacturer,
+                    ProductCost = products[i].ProductCost.ToString(),
+                    ProductQuantityInStock = products[i].ProductQuantityInStock.ToString()
+                });
+            }
+            productsListBox.ItemsSource = productForLists;
         }
+
+        private void EditProduct_Click(object sender, RoutedEventArgs e)
+        {
+            ProductForList selectedItemTest = (ProductForList)productsListBox.SelectedItem;
+            if (selectedItemTest == null)
+            {
+                MessageBox.Show("Выберите объект");
+                return;
+            }
+            selectedItem = SourceCore.entities1.Product.First(i => i.ProductArticleNumber == selectedItemTest.ProductId);
+            ProductNameTextBox.Text = selectedItem.ProductName;
+            ProductDescriptionTextBox.Text = selectedItem.ProductDescription;
+            ProductCategoryTextBox.Text = selectedItem.ProductCategory;
+            ProductManufactureTextBox.Text = selectedItem.ProductManufacturer;
+            ProductCostTextBox.Text = selectedItem.ProductCost.ToString();
+            ProductDiscountAmountTextBox.Text = selectedItem.ProductDiscountAmount.ToString();
+            ProductQuantityInStockTextBox.Text = selectedItem.ProductQuantityInStock.ToString();
+            ProductStatusTextBox.Text = selectedItem.ProductStatus;
+            actionName.Content = "Редактирование товара";
+            ActionColumn.Width = new GridLength(400);
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            if (justChange)
+            {
+                var product = new Base.Product();
+                product = SourceCore.entities1.Product.First(i => i.ProductArticleNumber == selectedItem.ProductArticleNumber);
+                product.ProductName = ProductNameTextBox.Text;
+                product.ProductDescription = ProductDescriptionTextBox.Text;
+                product.ProductCategory = ProductCategoryTextBox.Text;
+                product.ProductManufacturer = ProductManufactureTextBox.Text;
+                product.ProductCost = Decimal.Parse(ProductCostTextBox.Text);
+                product.ProductDiscountAmount = Byte.Parse(ProductDiscountAmountTextBox.Text);
+                product.ProductQuantityInStock = int.Parse(ProductQuantityInStockTextBox.Text);
+                product.ProductStatus = ProductStatusTextBox.Text;
+            }
+            else
+            {
+                var product = new Base.Product();
+                product.ProductName = ProductNameTextBox.Text;
+                product.ProductDescription = ProductDescriptionTextBox.Text;
+                product.ProductCategory = ProductCategoryTextBox.Text;
+                product.ProductManufacturer = ProductManufactureTextBox.Text;
+                product.ProductCost = Decimal.Parse(ProductCostTextBox.Text);
+                product.ProductDiscountAmount = Byte.Parse(ProductDiscountAmountTextBox.Text);
+                product.ProductQuantityInStock = int.Parse(ProductQuantityInStockTextBox.Text);
+                product.ProductStatus = ProductStatusTextBox.Text;
+                SourceCore.entities1.Product.Add(product);
+            }
+
+
+            try
+            {
+                SourceCore.entities1.SaveChanges();
+                ActionColumn.Width = new GridLength(0);
+                loadProducts();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void Cance_Click(object sender, RoutedEventArgs e)
+        {
+            ActionColumn.Width = new GridLength(0);
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            actionName.Content = "Добавление товара";
+            ActionColumn.Width = new GridLength(400);
+            justChange = false;
+        }
+
     }
 }
